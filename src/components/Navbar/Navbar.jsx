abf8514e60101f4d247c9f967e5dbe0d0231a36a
alessandro <alessandrodamata6@gmail.com>
2023-08-17 19:48:19 -0300

    configurando a tela de login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,10 @@ const Navbar = () => {
             <NavLink className={styles.brand} to="/">Micro <span>Blog</span></NavLink>
             <ul className={styles.links_list}>
                 <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/"}>Home</NavLink></li>
+                <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/login"}>Entrar</NavLink></li>
+                <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/register"}>Cadastrar</NavLink></li>
                 <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/about"}>Sobre</NavLink></li>
+
             </ul>
         </nav>
     );
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,69 @@
+import styles from "./Register.module.css";
+
+import { useState, useEffect } from "react";
+
+const Register = () => {
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [error, setError] = useState("");
+
+    const handleSubmit = (evento) => {
+        evento.preventDefault();
+        
+
+        setError("");
+
+        const user ={
+            displayName: displayName,
+            email: email,
+            password: password
+        };
+
+        if(password !== confirmPassword) {
+            setError("As senha devem ser iguais!");
+            return;
+        }
+
+        console.log(user);
+
+        setDisplayName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+    };
+    return (
+        <div className={styles.register}>
+            <h1>Cadastre-se para postar! </h1>
+            <p>Crie sua conta e compartilhe suas histórias</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Nome:</span>
+                    <input type="text" name="displayName" value={displayName} onChange={(e) => setDisplayName(e.target.value)} required placeholder="Nome de usuário" />
+                </label>
+
+                <label>
+                    <span>E-mail:</span>
+                    <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="E-mail do usuário" />
+                </label>
+
+                <label>
+                    <span>Senha:</span>
+                    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Digite sua senha" />
+                </label>
+
+                <label>
+                    <span>Confirmar senha:</span>
+                    <input type="password" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required placeholder="Confirme a sua senha" />
+                </label>
+
+                <button className="btn">Cadastrar</button>
+                {error && <p className="error">{error}</p>}
+            </form>
+        </div>
+    );
+};
+
+export default Register;
