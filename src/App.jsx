ab765b201e8feecb247ec2cf375d4cb12b0610e1
alessandro <alessandrodamata6@gmail.com>
2023-08-17 18:18:53 -0300

    configurando o env no vite, adicionando componentes navbar e footer, estilizando esses componentes e adicioanado estilizaçoes globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
-import "dotenv/config";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Navbar />
                 <div className="container">
                     <Routes>
                         <Route path="/"  element={<Home />} />
                         <Route path="/about"  element={<About />} />
                     </Routes>
                 </div>
+                <Footer />
             </BrowserRouter>
         </div>
     );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <NavLink className={styles.brand} to="/">Micro <span>Blog</span></NavLink>
+            <ul className={styles.links_list}>
+                <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/"}>Home</NavLink></li>
+                <li><NavLink className={({isActive}) => ( isActive ? styles.active : "" )} to={"/about"}>Sobre</NavLink></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
