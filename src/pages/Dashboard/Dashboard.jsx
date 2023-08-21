19b76de7df45c7a2f6ea44a41aed076696284592
alessandro <alessandrodamata6@gmail.com>
2023-08-21 16:52:58 -0300

    Finalizado a estilização da deshboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,45 @@
+import { Link } from "react-router-dom";
 import styles from "./Dashboard.module.css";
+import {useAuthValue} from "../../context/AuthContext";
+import {useFetchDocuments} from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
+    const {user} = useAuthValue();
+    const uid = user.uid;
+
+    const {documents: posts, loading, error} = useFetchDocuments("posts", null, uid);
+    const deleteDocument = (id) => {
+        
+    };
+
+    if(loading) {
+        return <p>Carregando...</p>;
+    }
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className={styles.dashboard}>
+            <h2>Dashboard</h2>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link className="btn " to="/posts/create">Criar primeiro post</Link>
+                </div>
+            ) : (
+                <>
+                    <div className={styles.post_header}>
+                        <span>Título</span>
+                        <span>Ações</span>
+                    </div>
+                    {posts && posts.map(post => <div className={styles.post_row} key={post.id}>
+                        <p>{post.title}</p>
+                        <div>
+                            <Link className="btn btn-outline" to={`/posts/${post.id}`}>Ver</Link>
+                            <Link className="btn btn-outline" to={`/posts/edit/${post.id}`}>Editar</Link>
+                            <button className="btn btn-outline btn-danger" onClick={() => deleteDocument(post.id)}>Excluir</button>
+                        </div>
+                    </div>)}
+                </>
+            )}
         </div>
     );
 };
